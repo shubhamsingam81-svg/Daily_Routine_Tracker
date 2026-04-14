616a169bf24fab7804ac836cc61fbbf0bd13e832
shubhamsingam81-svg <shubhamsingam81@gmail.com>
2026-04-14 23:01:12 +0530

    updated notification setting

--- sw.js
+++ sw.js
@@ -36,3 +36,34 @@ self.addEventListener('fetch', (event) => {
     }).catch(() => caches.match('./index.html'))
   );
 });
+
+// Handle notification show request from main page
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
+    const { title, body, tag } = event.data;
+    self.registration.showNotification(title, {
+      body: body,
+      icon: './icons/icon-192.svg',
+      badge: './icons/icon-192.svg',
+      tag: tag,
+      vibrate: [200, 100, 200, 100, 200],
+      requireInteraction: true,
+      actions: [{ action: 'open', title: 'Open App' }, { action: 'done', title: 'Mark Done' }]
+    });
+  }
+});
+
+// Handle notification click
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clients) => {
+      if (clients.length > 0) {
+        clients[0].focus();
+        clients[0].postMessage({ type: 'NOTIFICATION_CLICKED', tag: event.notification.tag, action: event.action });
+      } else {
+        self.clients.openWindow('./');
+      }
+    })
+  );
+});
